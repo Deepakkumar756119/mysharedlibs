94d4dfc3324624a91315831f101583ca7347aa6d
Deepakkumar756119 <135266815+Deepakkumar756119@users.noreply.github.com>
2023-08-25 13:00:14 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -8,12 +8,3 @@ def mavenBuild()
     sh 'mvn package'
 }
 
-def tomcatDeploy(jobname,ip,context)
-{
-   sh "scp /var/lib/jenkins/workspace/${jobname}/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat9/webapps/${context}.war"
-}
-
-def runSelenium(jobname)
-{
-   sh "java -jar /var/lib/jenkins/workspace/${jobname}/testing.jar"
-}
